a7aef897a33e1affc82f7091acbd7aa795431b92
Johnson Ho <m97_j@DESKTOP-QUMH9HN>
2016-12-02 23:29:09 +0100

    1.0

--- AoC/Program.cs
+++ AoC/Program.cs
@@ -10,13 +10,16 @@ namespace AoC
     {
         static string input = "L5, R1, L5, L1, R5, R1, R1, L4, L1, L3, R2, R4, L4, L1, L1, R2, R4, R3, L1, R4, L4, L5, L4, R4, L5, R1, R5, L2, R1, R3, L2, L4, L4, R1, L192, R5, R1, R4, L5, L4, R5, L1, L1, R48, R5, R5, L2, R4, R4, R1, R3, L1, L4, L5, R1, L4, L2, L5, R5, L2, R74, R4, L1, R188, R5, L4, L2, R5, R2, L4, R4, R3, R3, R2, R1, L3, L2, L5, L5, L2, L1, R1, R5, R4, L3, R5, L1, L3, R4, L1, L3, L2, R1, R3, R2, R5, L3, L1, L1, R5, L4, L5, R5, R2, L5, R2, L1, L5, L3, L5, L5, L1, R1, L4, L3, L1, R2, R5, L1, L3, R4, R5, L4, L1, R5, L1, R5, R5, R5, R2, R1, R2, L5, L5, L5, R4, L5, L4, L4, R5, L2, R1, R5, L1, L5, R4, L3, R4, L2, R3, R3, R3, L2, L2, L2, L1, L4, R3, L4, L2, R2, R5, L1, R2";
 
+        static HashSet<string> coords = new HashSet<string>();
+
+        static int dir, x, y;
+
         static void Main(string[] args)
         {
-
-            int dir = 0;
-            int x = 0;
-            int y = 0;
-
+            dir = 0;
+            x = 0;
+            y = 0;
+            int steps = 0;
             var inputs = input.Split(',');
             foreach (var s in inputs)
             {
@@ -25,25 +28,49 @@ namespace AoC
                     dir = (dir == 3) ? 0 : dir + 1;
                 else
                     dir = (dir == 0) ? 3 : dir - 1;
+                steps = int.Parse(str.Substring(1));
 
+                if (Move(steps).Length > 0)
+                    break;
+            }
+            Console.WriteLine(Distance(x, y));
+        }
+
+        static string Move(int steps)
+        {
+            while (steps > 0)
+            {
+                steps--;
                 switch (dir)
                 {
                     case 0:
-                        y += int.Parse(str.Substring(1));
+                        y++;
                         break;
                     case 1:
-                        x += int.Parse(str.Substring(1));
+                        x++;
                         break;
                     case 2:
-                        y -= int.Parse(str.Substring(1));
+                        y--;
                         break;
                     case 3:
-                        x -= int.Parse(str.Substring(1));
+                        x--;
                         break;
                 }
-
+                var str = CoordString(x, y);
+                if (coords.Contains(str))
+                {
+                    Console.WriteLine(str);
+                    return str;
+                }
+                else
+                    coords.Add(str);
             }
-            Console.WriteLine(Distance(x,y));
+            return "";
+        }
+
+        static string CoordString(int x, int y)
+        {
+            return $"{x},{y}";
         }
 
         static int Distance(int x, int y)
