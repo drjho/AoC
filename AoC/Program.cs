872d3ae5795df38c5824487d6d6b432dbf200707
Johnson Ho <m97_j@DESKTOP-QUMH9HN>
2016-12-02 23:05:24 +0100

    0.5

--- AoC/Program.cs
+++ AoC/Program.cs
@@ -8,8 +8,48 @@ namespace AoC
 {
     class Program
     {
+        static string input = "L5, R1, L5, L1, R5, R1, R1, L4, L1, L3, R2, R4, L4, L1, L1, R2, R4, R3, L1, R4, L4, L5, L4, R4, L5, R1, R5, L2, R1, R3, L2, L4, L4, R1, L192, R5, R1, R4, L5, L4, R5, L1, L1, R48, R5, R5, L2, R4, R4, R1, R3, L1, L4, L5, R1, L4, L2, L5, R5, L2, R74, R4, L1, R188, R5, L4, L2, R5, R2, L4, R4, R3, R3, R2, R1, L3, L2, L5, L5, L2, L1, R1, R5, R4, L3, R5, L1, L3, R4, L1, L3, L2, R1, R3, R2, R5, L3, L1, L1, R5, L4, L5, R5, R2, L5, R2, L1, L5, L3, L5, L5, L1, R1, L4, L3, L1, R2, R5, L1, L3, R4, R5, L4, L1, R5, L1, R5, R5, R5, R2, R1, R2, L5, L5, L5, R4, L5, L4, L4, R5, L2, R1, R5, L1, L5, R4, L3, R4, L2, R3, R3, R3, L2, L2, L2, L1, L4, R3, L4, L2, R2, R5, L1, R2";
+
         static void Main(string[] args)
         {
+
+            int dir = 0;
+            int x = 0;
+            int y = 0;
+
+            var inputs = input.Split(',');
+            foreach (var s in inputs)
+            {
+                var str = s.Trim();
+                if (str[0] == 'R')
+                    dir = (dir == 3) ? 0 : dir + 1;
+                else
+                    dir = (dir == 0) ? 3 : dir - 1;
+
+                switch (dir)
+                {
+                    case 0:
+                        y += int.Parse(str.Substring(1));
+                        break;
+                    case 1:
+                        x += int.Parse(str.Substring(1));
+                        break;
+                    case 2:
+                        y -= int.Parse(str.Substring(1));
+                        break;
+                    case 3:
+                        x -= int.Parse(str.Substring(1));
+                        break;
+                }
+
+            }
+            Console.WriteLine(Distance(x,y));
+        }
+
+        static int Distance(int x, int y)
+        {
+            Console.WriteLine(x + "," + y);
+            return Math.Abs(x) + Math.Abs(y);
         }
     }
 }
