cb16a28551c73b909620678c9b1bd84467384a6f
Johnson Ho <m97_j@DESKTOP-QUMH9HN>
2016-12-03 12:43:56 +0100

    3.0

--- AoC/Puzzles/D3.cs
+++ AoC/Puzzles/D3.cs
@@ -14,12 +14,53 @@ namespace AoC.Puzzles
 
         public D3()
         {
+            //One();
+            Two();
+        }
+
+        void One()
+        {
             var inputs = File.ReadAllLines(path);
             //Console.WriteLine(inputs.Length);
             int count = 0;
             foreach (var i in inputs)
             {
                 var nums = Regex.Split(i.Trim(), @"\D+").Select(e => int.Parse(e)).OrderBy(e => e).ToArray();
+                if (nums[0] + nums[1] > nums[2])
+                    ++count;
+            }
+            Console.WriteLine(count);
+        }
+
+
+        void Two()
+        {
+            var inputs = File.ReadAllLines(path);
+            int count = 0;
+            var list = new List<int[]>();
+            for (int i = 0; i < inputs.Length; i += 3)
+            {
+                var n1 = new int[3];
+                var n2 = new int[3];
+                var n3 = new int[3];
+                for (int j = 0; j < 3; j++)
+                {
+
+                    var nums = Regex.Split(inputs[i + j].Trim(), @"\D+").Select(e => int.Parse(e)).ToArray();
+                    n1[j] = nums[0];
+                    n2[j] = nums[1];
+                    n3[j] = nums[2];
+
+                }
+                list.Add(n1);
+                list.Add(n2);
+                list.Add(n3);
+                //break;
+            }
+            foreach (var nums in list)
+            {
+                Array.Sort(nums);
+                //Console.WriteLine(string.Join(",", nums));
                 if (nums[0] + nums[1] > nums[2])
                     ++count;
             }
